442cd7ce61c813085b884ae36b59f1393c2ab007
mktosos <taniguchidesigns@hotmail.com>
2018-12-25 01:30:11 -0800

    added hangman underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,10 +18,20 @@ if (guessWord===randomWord) {
     
 document.getElementById('winTally').textContent=wins;
     
-//random number generator to pick woord from array[i]
+//random number generator to pick fish from array - fish[i]
 var fish = ['bass', 'carp', 'catfish', 'bluegill', 'crappie', 'salmon', 'perch']
 var i = Math.floor(Math.random()*7)
 var guessWord=fish[i];
-console.log(i)
-console.log(guessWord)
-    
+console.log(i);
+console.log(guessWord);
+
+
+//create blak spaces for guessWord,  to add correct guesses use DOM to appropriate blnkSpcs[]
+var x = guessWord.length;
+    console.log(x)
+for (let index = 0; index <x; index++) {
+    //append x number of ___ ___ ___ ___
+var blnkSpcs=document.getElementById("blankSpaces");
+var underscore = document.createTextNode("___ ");
+blnkSpcs.appendChild(underscore);
+}
